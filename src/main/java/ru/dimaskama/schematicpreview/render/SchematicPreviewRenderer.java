81b224a5e64659c0cb34b61b2718ba95b9caba01
Mynam <mynameisiphone10@gmail.com>
2025-03-08 22:24:10 +0500

    0.0.10+1.21.4 Fix shifted schematics render in world

--- src/main/java/ru/dimaskama/schematicpreview/render/SchematicPreviewRenderer.java
+++ src/main/java/ru/dimaskama/schematicpreview/render/SchematicPreviewRenderer.java
@@ -163,6 +163,10 @@ public class SchematicPreviewRenderer implements AutoCloseable {
             buffer.bind();
             target.beginWrite(true);
             buffer.draw(RenderSystem.getModelViewMatrix(), RenderSystem.getProjectionMatrix(), shader);
+            if (shader.modelOffset != null) {
+                shader.modelOffset.set(0.0F, 0.0F, 0.0F);
+                shader.modelOffset.upload();
+            }
         });
         layer.endDrawing();
     }
